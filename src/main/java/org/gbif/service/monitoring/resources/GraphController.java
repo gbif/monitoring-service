c360e4dad9ce470aa911c962207895c55cc11d74
fmendezh <federicomh@gmail.com>
2020-10-22 10:06:20 +0200

    making all view endpoints working

--- src/main/java/org/gbif/service/monitoring/resources/GraphController.java
+++ src/main/java/org/gbif/service/monitoring/resources/GraphController.java
@@ -4,17 +4,13 @@ import org.gbif.service.monitoring.model.Platform;
 import org.gbif.service.monitoring.model.TreeNode;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GraphController {
@@ -26,13 +22,11 @@ public class GraphController {
   private static final ObjectMapper MAPPER = new ObjectMapper();
 
   @GetMapping(value = "/graph")
-  public String getGraph(Model model){
+  public String getGraph(Model model) {
     try {
-      Map<String, Object> map = new HashMap<>();
       Platform platform = new Platform();
       platform.setName("GBIF");
       platform.setEnvironments(environmentsMonitorResource.getEnvironments());
-      map.put("data", MAPPER.writeValueAsString(TreeNode.toD3Node(platform)));
       model.addAttribute("data", MAPPER.writeValueAsString(TreeNode.toD3Node(platform)));
       return "graph";
     } catch(IOException ex){
@@ -41,22 +35,20 @@ public class GraphController {
   }
 
   @GetMapping(value = "graph/{env}")
-  public ModelAndView getGraph(@PathVariable("env") String env){
+  public String getGraph(@PathVariable("env") String env, Model model) {
     try {
-      Map<String, Object> map = new HashMap<>();
-      map.put("data", MAPPER.writeValueAsString(TreeNode.toD3Node(environmentsMonitorResource.getEnvironment(env))));
-      return new ModelAndView("/graph",map);
+      model.addAttribute("data", MAPPER.writeValueAsString(TreeNode.toD3Node(environmentsMonitorResource.getEnvironment(env))));
+      return "graph";
     } catch(IOException ex){
       throw new RuntimeException(ex);
     }
   }
 
-  @GetMapping(value = "graph/{env}/{service}", produces = MediaType.TEXT_HTML_VALUE)
-  public ModelAndView getGraph(@PathVariable("env") String env, @PathVariable("service") String serviceName){
+  @GetMapping(value = "graph/{env}/{service}")
+  public String getGraph(@PathVariable("env") String env, @PathVariable("service") String serviceName, Model model) {
     try {
-      Map<String, Object> map = new HashMap<>();
-      map.put("data", MAPPER.writeValueAsString(TreeNode.toD3Node(environmentsMonitorResource.getService(env, serviceName))));
-      return new ModelAndView("/graph",map);
+      model.addAttribute("data", MAPPER.writeValueAsString(TreeNode.toD3Node(environmentsMonitorResource.getService(env, serviceName))));
+      return "graph";
     } catch(IOException ex){
       throw new RuntimeException(ex);
     }
